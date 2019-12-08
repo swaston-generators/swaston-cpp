35202a51d175a3b029bfa2b8fe79a5d563a1cabf
Wirtos <Wirtos.new@gmail.com>
2019-12-08 19:29:52 +0200

    headers-only solution, code cleanup, optimisation of generator algorithm, updated utf8_tools header

--- include/swaston_generator.h
+++ include/swaston_generator.h
@@ -1,8 +1,68 @@
-#ifndef SWASTON_GENERATOR
-#define SWASTON_GENERATOR
+#ifndef SWASTON_GENERATOR_H
+#define SWASTON_GENERATOR_H
 
 #include <string>
+#include "utf8.h"
+#include "utf8_tools.h"
 
-std::string generate_swaston(const std::string& s);
+std::string generate_swaston(const std::string &s) {
+    if (utf8::distance(s.begin(), s.end()) < 2) { // can't use distance smaller than 2 because size_t underflow
+        return s;
+    }
+    std::string
+            l_spaced_word = utf8_join(s, " "),
+            r_spaced_word = utf8_reverse(utf8_join(s, " ")),
+            l_word = utf8_reverse(s),
+            center = l_spaced_word + utf8_substr(r_spaced_word, 1) + '\n',
+            tab_pre = utf8_repeat(" ", utf8::distance(r_spaced_word.begin(), r_spaced_word.end()) - size_t(2)),
+            tab_post = tab_pre + " ", // extended tab
+            upper,
+            lower,
+            final;
+
+    std::vector<std::string>
+            r_vec = utf8_to_vector(s), // s == left-to-right word
+            l_vec = utf8_to_vector(l_word); // convert utf-8 string to vector of strings
+
+    size_t size = l_vec.size();
+
+    for (size_t c = 0; c != size - 1; c++) { // upper side
+
+        if (c == 0) { // case first row
+            upper += l_vec[c];
+            upper += tab_pre;
+            upper += l_spaced_word;
+            upper += '\n';
+        } else {
+            upper += l_vec[c];
+            upper += tab_pre;
+            upper += r_vec[c];
+            upper += tab_post;
+            upper += '\n';
+
+        }
+    }
+
+    for (size_t c = 1; c != size; c++) { // lower side
+        if (c == size - 1) { // case last row
+            lower += r_spaced_word;
+            lower += tab_pre;
+            lower += r_vec[c];
+            lower += '\n';
+        } else {
+            lower += tab_pre;
+            lower += ' ';
+            lower += l_vec[c];
+            lower += tab_pre;
+            lower += r_vec[c];
+            lower += '\n';
+        }
+    }
+
+    final.append(upper);
+    final.append(center);
+    final.append(lower);
+    return final;
+}
 
 #endif
--- include/utf8_tools.h
+++ include/utf8_tools.h
@@ -1,21 +1,212 @@
-#ifndef utf8_tools
-#define utf8_tools
+#ifndef UTF8_TOOLS_H
+#define UTF8_TOOLS_H
 
+#include "utf8_tools.h"
+#include "utf8.h"
+#include <stdint.h>
 #include <string>
 #include <vector>
 
-std::string utf8_join(const std::string &, const std::string & = ", ");
+#ifdef _WIN32
 
-std::string utf8_reverse(const std::string &);
+#include <io.h>
+#include <fcntl.h>
 
-std::string utf8_substr(const std::string &, size_t, size_t = 0);
+#endif
 
-std::string utf8_repeat(const std::string &, size_t);
+std::string utf8_join(const std::string &s, const std::string &separator) {
+    std::string result;
+    std::string::const_iterator begin = s.begin();
+    std::string::const_iterator end = s.end();
+    uint32_t cp = 0;
 
-std::vector<std::string> utf8_to_vector(const std::string &);
+    while (true) {
+        cp = utf8::next(begin, end);
+        utf8::append(cp, std::back_inserter(result));
 
-std::wstring utf8string_to_utf16_wstring(const std::string &);
+        if (begin != end) {
+            result.append(std::string(separator));
+        } else {
+            break;
+        }
 
-std::string utf16wstring_to_utf8string(const std::wstring &);
+    }
+    return result;
+
+}
+
+std::string utf8_reverse(const std::string &s) {
+    std::string result;
+    std::string::const_iterator begin = s.begin();
+    std::string::const_iterator end = s.end();
+    uint32_t cp = 0;
+
+    while (begin != end) {
+        cp = utf8::prior(end, begin);
+        utf8::append(cp, std::back_inserter(result));
+    }
+
+    return result;
+}
+
+
+std::string utf8_substr(const std::string &s, size_t st, size_t en = 0) {
+    if (st == 0 && en == 0) {
+        return std::string();
+    }
+    if (en > 0 && st > en) {
+        throw std::out_of_range("Start index is bigger than end");
+    }
+
+    size_t distance = utf8::distance(s.begin(), s.end());
+
+    if (st > distance || en > distance) {
+        throw std::out_of_range("Index is greater than the string length");
+    }
+
+    std::string result;
+    std::string::const_iterator begin = s.begin();
+    std::string::const_iterator end = s.end();
+    uint32_t cp = 0;
+    size_t counter = 0;
+
+    for (; counter < st; counter++) { // skip string up to start value
+        utf8::next(begin, end);
+    }
+    while (begin != end) {
+        if (en == 0 || counter + 1 < en) {
+            cp = utf8::next(begin, end);
+            utf8::append(cp, std::back_inserter(result));
+        } else if (counter == en) {
+            cp = utf8::next(begin, end);
+            utf8::append(cp, std::back_inserter(result));
+            break;
+        }
+        counter++;
+
+    }
+    return result;
+}
+
+std::string utf8_repeat(const std::string &s, size_t n) {
+    std::string result;
+    for (size_t c = 1; c <= n; c++) {
+        result.append(s);
+    }
+    return result;
+}
+
+std::vector<std::string> utf8_to_vector(const std::string &s) {
+    std::string::const_iterator begin = s.begin();
+    std::string::const_iterator end = s.end();
+
+    std::vector<std::string> vec;
+
+    uint32_t cp = 0;
+    while (begin != end) {
+        cp = utf8::next(begin, end);
+        std::string result;
+        utf8::append(cp, std::back_inserter(result));
+        vec.push_back(result);
+    }
+    return vec;
+}
+
+
+std::wstring utf8string_to_utf16_wstring(const std::string &s) {
+    std::wstring result;
+    utf8::utf8to16(s.begin(), s.end(), std::back_inserter(result));
+    return result;
+}
+
+std::string utf16wstring_to_utf8string(const std::wstring &ws) {
+    std::string result;
+    utf8::utf16to8(ws.begin(), ws.end(), std::back_inserter(result));
+    return result;
+}
+
+
+void utf8_print(const std::string &s) {
+#ifdef _WIN32
+    int mode = _setmode(_fileno(stdout), _O_U16TEXT);
+    if (mode == -1) {
+        throw std::runtime_error("Cannot set output mode to UTF-16");
+    }
+    fwprintf(stdout, L"%s", utf8string_to_utf16_wstring(s).c_str());
+#else
+    fprintf(stdout, "%s", s.c_str());
+#endif
+}
+
+std::string utf8_getstring() {
+#ifdef _WIN32 // platform-specific code
+
+#define chartype wchar_t
+#define end_of_file WEOF
+#define strtype(s) L ## s
+#define inttype  wint_t
+#define unigetchar() getwchar()
+
+    int mode = _setmode(_fileno(stdin), _O_U16TEXT);
+    if (mode == -1) {
+        throw std::runtime_error("Cannot set input mode to UTF-16");
+    }
+
+#else
+#define chartype char
+#define end_of_file EOF
+#define strtype(s) s
+#define inttype  int
+#define unigetchar() getchar()
+
+#endif // ~platform-specific code
+    size_t buf_size = 128;
+    size_t current_size = 0;
+
+    chartype *p_str = (chartype *) (malloc(sizeof(chartype) * buf_size));
+    current_size = buf_size;
+
+
+    if (p_str == NULL) {
+        throw std::bad_alloc();
+    }
+    inttype in; // wint_t if windows, else int
+    chartype ch; // wchar_t if windows, else char
+    size_t i = 0;
+
+    while (true) {
+        in = unigetchar();
+        if (in == strtype('\n') ||  // unigetchar - getwchar if windows, else getchar
+            (in == end_of_file)) {
+            break;
+        }
+        ch = in;
+        p_str[i] = (chartype) ch;
+        i++;
+
+        // if i reached maximize size then realloc size
+        if (i == current_size) {
+            current_size = i + buf_size;
+            p_str = (chartype *) realloc(p_str, sizeof(chartype) * current_size);
+            if (p_str == NULL) {
+                throw std::bad_alloc();
+            }
+        }
+
+    }
+
+    p_str[i] = strtype('\0');
+#ifdef _WIN32
+    std::string s(utf16wstring_to_utf8string((chartype *) p_str));
+#else
+    std::string s((chartype*)p_str);
+
+#endif
+//free it
+    free(p_str);
+    return s;
+
+}
+
 
 #endif
